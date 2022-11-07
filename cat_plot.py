12720569b982caac71072438f6aaaedf49e82d62
anisawan92 <49561083+anisawan92@users.noreply.github.com>
2022-11-07 20:26:23 +0500

    commit it

--- cat_plot.py
+++ cat_plot.py
@@ -1,9 +1,8 @@
-# scatter plot
+# cat plot
 import seaborn as sns
 import matplotlib.pyplot as plt
 sns.set_theme(style="ticks", color_codes=True)
 titanic = sns.load_dataset("titanic")
-g=sns.FacetGrid(titanic, row="sex", hue="alone")
-g=(g.map(plt.scatter, "age", "fare").add_legend())
+sns.catplot(x="sex", y="survived", hue="class", data=titanic)
+# g=(g.map(plt.scatter, "age", "fare").add_legend())
 plt.show()
-
