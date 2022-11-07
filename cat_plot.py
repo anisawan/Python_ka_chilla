6cae8659c973504161f2b9b39a48f92237362521
anisawan92 <49561083+anisawan92@users.noreply.github.com>
2022-11-07 20:28:54 +0500

    commit it

--- cat_plot.py
+++ cat_plot.py
@@ -3,6 +3,6 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 sns.set_theme(style="ticks", color_codes=True)
 titanic = sns.load_dataset("titanic")
-sns.catplot(x="sex", y="survived", hue="class", data=titanic)
+sns.catplot(x="sex", y="survived", hue="class", kind="bar", data=titanic)
 # g=(g.map(plt.scatter, "age", "fare").add_legend())
 plt.show()
