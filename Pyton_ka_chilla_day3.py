9f0a4dd87fde19ab12627ed312d3033972391d7b
anisawan92 <49561083+anisawan92@users.noreply.github.com>
2022-11-07 19:45:52 +0500

    Initial commit

---
+++ Pyton_ka_chilla_day3.py
@@ -0,0 +1,5 @@
+import seaborn as sns
+import matplotlib.pyplot as plt
+sns.set_theme(style="ticks", color_codes=True)
+titanic = sns.load_dataset("titanic")
+g=sns.FacetGrid(titanic, row="sex", hue="")
